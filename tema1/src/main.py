e0139bc946587eebee0e2a35f2868e2d2b2b526d
Luca1152 <t.luca1152@gmail.com>
2022-05-06 16:29:24 +0300

    Add CLI

--- tema1/src/main.py
+++ tema1/src/main.py
@@ -1,7 +1,28 @@
+import argparse
 import os
+from pathlib import Path
 
 from tema1.src.game.Problema import Problema
 
-parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-p = Problema(parent_dir + "/input/simplu.in", parent_dir + "/output/simplu-output.out")
-p.rezolva()
+
+def setup_cli():
+    parser = argparse.ArgumentParser(description="Rezolva problema 'Șoareci și pisici'.")
+    parser.add_argument("-i", "--input", metavar="PATH",
+                        help="calea absoluta catre folderul de input", required=True)
+    parser.add_argument("-o", "--output", metavar="PATH",
+                        help="calea absoluta catre folderul de output", required=True)
+    parser.add_argument("-n", metavar="SOLUTIONS", type=int,
+                        help="numarul de solutii de calculat", required=True)
+    parser.add_argument("-t", "--timeout", metavar="S", type=float,
+                        help="timpul de timeout per solutie, in secunde", required=True)
+
+    args = parser.parse_args()
+
+    for input_file_name in os.listdir(args.input):
+        input_file_path = args.input + "/" + input_file_name
+        file_name = str(Path(input_file_name).stem)
+        p = Problema(input_file_path, args.output + "/" + file_name + "-output.out")
+        p.rezolva()
+
+
+setup_cli()
